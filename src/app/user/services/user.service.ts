7ffd4808a0c21d1d0622c49e425b8c2bef122329
matheuscla <matheuscla@yahoo.com.br>
2017-10-11 15:15:48 -0300

    Post for user and toast service

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { API } from './../../app.api.definitions';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class UserService {
@@ -10,5 +11,11 @@ export class UserService {
   getUsers() {
     return this.http.get(`${API.domain}/users`)
       .map(res => res.json());
+  }
+
+  createUser(user) {
+    return this.http.post(`${API.domain}/users`, user)
+      .map(res => res.statusText)
+      .toPromise();
   }
 }
--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { UserService } from "../services/user.service";
+import { MzToastService } from "ng2-materialize/dist";
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
   public userForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    public userService: UserService,
+    private toastService: MzToastService
+  ) {
+
     this.userForm = formBuilder.group({
       'name': ['', Validators.required],
       'username': ['', Validators.required],
       'email': ['', Validators.required],
       'phone': ['', Validators.required],
       'website': ['', Validators.required],
+      'street': ['', Validators.required],
+      'suite': ['', Validators.required],
+      'city': ['', Validators.required],
+      'zip': ['', Validators.required],
+      'company_name': ['', Validators.required],
+      'catchphrase': ['', Validators.required],
+      'bs': [''],      
     })
    }
 
@@ -23,7 +37,31 @@ export class UserFormComponent implements OnInit {
   }
 
   onSubmit(form) {
-    console.log(form);
+    const user = {
+      'name': form.name,
+      'username': form.username,
+      'email': form.email,
+      'phone': form.phone,
+      'website': form.website,
+      'address': {
+        'street': form.street,
+        'suite': form.suite,
+        'city': form.city,
+        'zipcode': form.zip
+      },
+      'company': {
+        'name': form.company_name,
+        'catchPhrase': form.catchphrase,
+        'bs': form.bs
+      }
+    }
+    
+    this.userForm.reset();
+    this.userService.createUser(user).then(res => {
+      if (res == 'OK') {
+        this.toastService.show('User created successfully!', 4000, 'green');
+      }
+    })
   }
 
 }
